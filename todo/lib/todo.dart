5e818d8bb8b77ddbd7a8944b258f932d34febd90
shigeru.hyodo <41170561+gel1123@users.noreply.github.com>
2022-09-27 05:38:59 +0900

    create todo app

---
+++ todo/lib/todo.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class Todo extends StatefulWidget {
+  const Todo({super.key});
+
+  @override
+  State<Todo> createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+  final List<String> _todoItems = [];
+
+  void _addTodoItem(String task) {
+    // Only add the task if the user actually entered something
+    if (task.length > 0) {
+      setState(() => _todoItems.add(task));
+    }
+  }
+
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Mark "${_todoItems[index]}" as done?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('CANCEL'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: const Text('MARK AS DONE'),
+              onPressed: () {
+                _removeTodoItem(index);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Add a new task'),
+            ),
+            body: TextField(
+              autofocus: true,
+              onSubmitted: (val) {
+                _addTodoItem(val);
+                Navigator.pop(context); // Close the add todo screen
+              },
+              decoration: const InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: EdgeInsets.all(16.0),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return _buildTodoItem(_todoItems[index], index);
+      },
+      itemCount: _todoItems.length,
+    );
+  }
+
+  Widget _buildTodoItem(String todoText, int index) {
+    return ListTile(
+      title: Text(todoText),
+      onTap: () => _promptRemoveTodoItem(index),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pushAddTodoScreen,
+        tooltip: 'Add task',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
